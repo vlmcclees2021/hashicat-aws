9fc66ae4d1a3d512e5ec05a6597436af53fa3720
Virinia McClees <virinia_x_mcclees@fanniemae.com>
2021-03-05 22:16:45 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -125,7 +125,8 @@ resource "aws_instance" "hashicat" {
     
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    department = "devops"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
